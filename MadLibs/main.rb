92d738b9723a07a861a77479e56759dfaa9c2d04
ChantalDemissie <42252976+ChantalDemissie@users.noreply.github.com>
2018-09-12 22:54:43 -0700

    Add files via upload

---
+++ MadLibs/main.rb
@@ -0,0 +1,21 @@
+My_name= "Chantal"
+Celebrity= "Jason Mamoa"
+#My favorite celebrity#
+Adjective= "Curly Hair"
+#my hair is very curly#
+Singular_noun= "The River"
+#i chose river since he is aquaman#
+Number= "7"
+Food= "Gumbo"
+
+puts "My name is " + My_name
+puts "One day i was by the " + Singular_noun
+puts "i ran into my celebrity crush " + Celebrity
+puts "i could not believe it was him! i exclaimed we have the same " + Adjective
+#curly hair is the best
+puts "He asked me what is your name"
+puts "i said " + My_name 
+puts "We then ate " + Food 
+# i make very good gumbo
+puts "i have had a crush on him since i was #{7}"
+puts "What a magical day at the ".upcase + Singular_noun.upcase
