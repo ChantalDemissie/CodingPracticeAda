0831d7de72c00727f3cce934a7cd82dfa7c872ad
ChantalDemissie <42252976+ChantalDemissie@users.noreply.github.com>
2018-09-12 22:34:01 -0700

    Add files via upload

---
+++ My dogs/main.rb
@@ -0,0 +1,13 @@
+dog_name= "Persephone"
+dog_age= 5
+puts "my dogs name is " + dog_name
+puts "And she is #{dog_age} years old."
+dog_name2= "Miffy"
+dog_age2= 7
+puts "my other dogs name is " + dog_name2
+puts" and he is #{dog_age2} years old"
+dog_breed= "Great Dane"
+dog_breed2= "Weener Dog"
+#persephone is my favorite
+puts "Persephone is a " + dog_breed
+puts "Miffy is a " + dog_breed2
