a159ecbc984d1853962fb2066245258cd4174b61
ChantalDemissie <42252976+ChantalDemissie@users.noreply.github.com>
2018-09-12 22:39:58 -0700

    Add files via upload

---
+++ AnimalGuessingGame/main.rb
@@ -0,0 +1,13 @@
+puts "Is the animal a feline, arachnid or bird ?"
+kind= gets.chomp
+#spider since its almost halloween
+puts "how many legs does it have"
+leg_count= gets.chomp
+#8
+puts " is the animal hairy, fuzzy or feathered?"
+feel= gets.chomp
+
+puts "the animal you described is a " + kind + "." 
+puts " it has #{leg_count} legs "
+puts "and it feels " + feel + "."
+#HappyHalloween!
